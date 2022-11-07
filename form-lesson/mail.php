e47fc5a90bf4421d0efb68f0e51e05a5ee37a5ca
Павлов Дмитрий Евгеньевич <dpavlov@niime.ru>
2022-11-07 15:02:31 +0300

    add new accordion at index

--- form-lesson/mail.php
+++ form-lesson/mail.php
@@ -1,25 +1,27 @@
 <?php 
 
-require_once('form-lesson/phpmailer/PHPMailerAutoload.php');
+require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
-$name = $_POST['user_name'];
-$phone = $_POST['user_phone'];
-$email = $_POST['user_email'];
+
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
 
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->Host = 'smtp.timeweb.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'dzharuzov@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = '$dk820&123'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'mail@cb90059.tmweb.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'Frontdeveloper1!'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('dzharuzov@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('gomudusu@p33.org');     // Кому будет уходить письмо 
+$mail->setFrom('mail@cb90059.tmweb.ru'); // от кого будет уходить письмо?
+$mail->addAddress('developavlov@yandex.ru');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
+// import JustValidate from '../node_modules/just-validate/dist/just-validate.es.js'
+
+
+//Popup container 
+
+(function(){
+  let button = document.querySelector('.header__nav-btn');
+  let popup = document.querySelector('.header__popup');
+  let closePopup = document.querySelector('.header__close');
+  let modal = document.querySelector('.header__pop-modal');
+  let body = document.getElementsByTagName('body');
+
+  document.querySelectorAll('.btn').forEach(function(tabsBtn){
+    tabsBtn.addEventListener('click', function(){
+      document.body.classList.add('disable-scroll');
+      popup.classList.add('show__popup');
+      popup.classList.add('fade');
+      popup.classList.add('animate-open');
+      modal.classList.add('is-open');
+    });
+  });
+  closePopup.addEventListener('click', function(){
+    popup.classList.remove('show__popup');
+    popup.classList.remove('fade');
+    popup.classList.remove('animate-open');
+    modal.classList.remove('is-open');
+    document.body.classList.remove('disable-scroll');
+  })
+})();
+
+let user = document.getElementById('#name');
+let phoneNum = document.getElementById('#phone');
+let email = document.getElementById('#email');
+let form = document.getElementById('#form');
+
+
 const swiper1 = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
@@ -28,7 +64,9 @@ const swiper1 = new Swiper('.swiper', {
 const swiper = new Swiper('.swiper1', {
     slidesPerView: 4,
     spaceBetween: 50,
+    direction: 'horizontal',
     loop: true,
+    effect: 'slide',
 
     breakpoints:{
       1200: {
@@ -56,6 +94,9 @@ const swiper = new Swiper('.swiper1', {
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: 'false',
+      totalClass: 'swiper-pagination-total',
     },
   
     // Navigation arrows
@@ -69,11 +110,115 @@ const swiper = new Swiper('.swiper1', {
       el: '.swiper-scrollbar',
     },
     autoplay: {
-      delay: 500,
+      delay: 5000,
     },
   });
 
- 
+  //accordion
+
+  let accordion = document.getElementById('accordion');
+accordion.addEventListener('click', change);
+function change(event) {
+  let targ = event.target;
+  if (targ.tagName !== 'H3') return;
+  if (targ.classList.contains('select__list')) {
+    hideAll();
+  } else {
+    hideAll();
+    targ.classList.add('select__list');
+    showText(targ.nextElementSibling);
+  }
+}
+function hideAll() {
+  let h3El = accordion.querySelectorAll('h3');
+  let divEl = accordion.querySelectorAll('div');
+  for (let i = 0; i < h3El.length; i++) {
+    h3El[i].classList.remove('select__list');
+  }
+  for (let i = 0; i < divEl.length; i++) {
+    divEl[i].style.height = '0';
+  }
+}
+function showText(textEl) {
+  textEl.style.height = textEl.scrollHeight + 'px';
+}
+
+//   VALIDATION
+
+//   const validation = new JustValidate(
+//     '#form',
+//     {
+//       errorFieldCssClass: 'is-invalid',
+//       errorFieldStyle: {
+//         border: '1px solid red',
+//       },
+//       errorLabelCssClass: 'is-label-invalid',
+//       errorLabelStyle: {
+//         color: 'red',
+//         textDecoration: 'underlined',
+//       },
+//       focusInvalidField: true,
+//       lockForm: true,
+//       tooltip: {
+//         position: 'top',
+//       },
+//       errorsContainer: '.errors-container',
+//     },
+//     [
+//       {
+//         key: 'Name is too short',
+//         dict: {
+//           ru: 'Имя слишком короткое',
+//           es: 'El nombre es muy corto',
+//         },
+//       },
+//       {
+//         key: 'Field is required',
+//         dict: {
+//           ru: 'Обязательное поле',
+//           es: 'Se requiere campo',
+//         },
+//       },
+//     ]
+//   );
+
+//   validation
+//   .addField('#name', [
+//     {
+//       rule: 'required'
+//     },
+//     {
+//       rule: 'minLength',
+//       value: 3,
+//     },
+//     {
+//       rule: 'maxLength',
+//       value: 30,
+//     },
+//   ])
+//   .addField('#email', [
+//     {
+//       rule: 'required',
+//       errorMessage: 'Email is required',
+//     },
+//     {
+//       rule: 'email',
+//       errorMessage: 'Email is invalid!',
+//     },
+//   ])
+//   .addField('#flexCheckDefault', [
+//     {
+//       rule: 'required',
+//     },
+//   ])
+
+   
+//   .onSuccess((event)=>{
+//  
+//  })
+   
+  
+
 // Clicking on links with closure Burger menu
 
 let menuEl = document.querySelectorAll('.header__nav-list');
@@ -87,136 +232,89 @@ for(menuEl of menuEl){
   })
 }
 
-//Popup container 
-
-(function(){
-  let button = document.querySelector('.header__nav-btn');
-  let popup = document.querySelector('.header__popup');
-  let closePopup = document.querySelector('.header__close');
-  let modal = document.querySelector('.header__pop-modal');
-  let body = document.getElementsByTagName('body');
-  document.querySelectorAll('.btn').forEach(function(tabsBtn){
-    tabsBtn.addEventListener('click', function(){
-      document.body.classList.add('disable-scroll');
-      popup.classList.add('show__popup');
-      popup.classList.add('fade');
-      popup.classList.add('animate-open');
-      modal.classList.add('is-open');
-    });
-  });
- 
-
-  closePopup.addEventListener('click', function(){
-    popup.classList.remove('show__popup');
-    popup.classList.remove('fade');
-    popup.classList.remove('animate-open');
-    modal.classList.remove('is-open');
-    document.body.classList.remove('disable-scroll');
-  })
-
-})();
-
-
-//yandex map
-
-// ymaps.ready(init);
-// var myMap, myGeoObject, myRectangle;
-
-// function init () {
-//     myMap = new ymaps.Map('map', {
-//         center: [55.674, 37.601],
-//         zoom: 11
-//     }, {
-//         searchControlProvider: 'yandex#search'
-//     });
-
-//     // Cоздаем геообъект с типом геометрии "прямоугольник".
-//     myGeoObject = new ymaps.GeoObject({
-//         // Геометрия = тип геометрии + координаты геообъекта.
-//         geometry: {
-//             // Тип геометрии - прямоугольник.
-//             type: 'Rectangle',
-//             // Координаты.
-//             coordinates: [
-//                 [55.665, 37.66],
-//                 [55.64, 37.53]
-//             ]
-//         },
-//         // Свойства.
-//         properties: {
-//             hintContent: 'Перетащи меня!',
-//             balloonContent: 'Прямоугольник 2'
-//         }
-//     }, {
-//         // Опции.
-//         // Объект можно перетаскивать.
-//         draggable: true,
-//         // Цвет и прозрачность заливки.
-//         fillColor: '#ffff0022',
-//         // Цвет и прозрачность границ.
-//         strokeColor: '#3caa3c88',
-//         // Ширина линии.
-//         strokeWidth: 7
-//     });
-
-//     // Создаем прямоугольник с помощью вспомогательного класса.
-//     myRectangle = new ymaps.Rectangle([
-//         // Задаем координаты диагональных углов прямоугольника.
-//         [55.66, 37.60],
-//         [55.71, 37.69]
-//     ], {
-//         //Свойства
-//         hintContent: 'Меня перетаскивать нельзя!',
-//         balloonContent: 'Прямоугольник 1'
-//     }, {
-//         // Опции.
-//         // Цвет и прозрачность заливки.
-//         fillColor: '#7df9ff33',
-//         // Дополнительная прозрачность заливки..
-//         // Итоговая прозрачность будет не #33(0.2), а 0.1(0.2*0.5).
-//         fillOpacity: 0.5,
-//         // Цвет обводки.
-//         strokeColor: '#0000FF',
-//         // Прозрачность обводки.
-//         strokeOpacity: 0.5,
-//         // Ширина линии.
-//         strokeWidth: 2,
-//         // Радиус скругления углов.
-//         // Данная опция принимается только прямоугольником.
-//         borderRadius: 6
-//     });
-
-//     myMap.geoObjects
-//         .add(myRectangle)
-//         .add(myGeoObject);
-// }
-
-
-
 ymaps.ready(init);
 
-var myMap;
+function init() {
+    var myMap = new ymaps.Map("map", {
+            center: [56.012229, 37.195346],
+            zoom: 14
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
 
-function init () {
-    // Инициализация вкладок.
-    // После исполнения команды tabs() tab-2 получит style='display:none'.
-    // Карта будет инициализирована, но будет иметь нулевой размер.
-    // В данном случае это хорошо, так как невидимая карта не будет загружать невидимые тайлы.
-    $('#tabs').tabs();
-    myMap = new ymaps.Map('tab-2', {
-        center: [56.012229, 37.195346], // Москва
-        zoom: 10
-    });
+    // Создаем геообъект с типом геометрии "Точка".
+        myGeoObject = new ymaps.GeoObject({
+            // Описание геометрии.
+            geometry: {
+                type: "Point",
+                coordinates: [56.012229, 37.195346]
+            },
+            // Свойства.
+            properties: {
+                // Контент метки.
+                iconContent: 'Prospect School'
+            }
+        }, {
+            // Опции.
+            // Иконка метки будет растягиваться под размер ее содержимого.
+            preset: 'islands#blackStretchyIcon',
+            // Метку можно перемещать.
+            draggable: true
+        })
 
-    // В момент показа новой вкладки будем пересчитывать размер карты.
-    // Карта примет максимально возможные значения при активации ее таба
-    // и нулевые, как только будет выбран первый таб.
-    // Подписываемся на событие 'tabsshow' (а не 'tabselect',
-    // так как требуется, чтобы элемент с картой уже был виден).
-    $('#tabs').bind('tabsshow', function (event, ui) {
-        myMap.container.fitToViewport();
-    });
+    myMap.geoObjects
+        .add(myGeoObject)
+        .add(myPieChart)
+        .add(new ymaps.Placemark([55.684758, 37.738521], {
+            balloonContent: 'цвет <strong>воды пляжа бонди</strong>'
+        }, {
+            preset: 'islands#icon',
+            iconColor: '#0095b6'
+        }))
+        .add(new ymaps.Placemark([55.833436, 37.715175], {
+            balloonContent: '<strong>серобуромалиновый</strong> цвет'
+        }, {
+            preset: 'islands#dotIcon',
+            iconColor: '#735184'
+        }))
+        .add(new ymaps.Placemark([55.687086, 37.529789], {
+            balloonContent: 'цвет <strong>влюбленной жабы</strong>'
+        }, {
+            preset: 'islands#circleIcon',
+            iconColor: '#3caa3c'
+        }))
+        .add(new ymaps.Placemark([55.782392, 37.614924], {
+            balloonContent: 'цвет <strong>детской неожиданности</strong>'
+        }, {
+            preset: 'islands#circleDotIcon',
+            iconColor: 'yellow'
+        }))
+        .add(new ymaps.Placemark([55.642063, 37.656123], {
+            balloonContent: 'цвет <strong>красный</strong>'
+        }, {
+            preset: 'islands#redSportIcon'
+        }))
+        .add(new ymaps.Placemark([55.826479, 37.487208], {
+            balloonContent: 'цвет <strong>фэйсбука</strong>'
+        }, {
+            preset: 'islands#governmentCircleIcon',
+            iconColor: '#3b5998'
+        }))
+        .add(new ymaps.Placemark([55.694843, 37.435023], {
+            balloonContent: 'цвет <strong>носика Гены</strong>',
+            iconCaption: 'Очень длиннный, но невероятно интересный текст'
+        }, {
+            preset: 'islands#greenDotIconWithCaption'
+        }))
+        .add(new ymaps.Placemark([55.790139, 37.814052], {
+            balloonContent: 'цвет <strong>голубой</strong>',
+            iconCaption: 'Очень длиннный, но невероятно интересный текст'
+        }, {
+            preset: 'islands#blueCircleDotIconWithCaption',
+            iconCaptionMaxWidth: '50'
+        }));
 }
+
 
 
   
